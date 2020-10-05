fbd8882b3b64ded0883176b90ced0d71ff5492ed
Karl Åberg <karl.aberg886@gmail.com>
2020-10-05 15:12:10 +0200

    Admin / FileHandler

--- RecepyWebsight/Classes/FileHandler.cs
+++ RecepyWebsight/Classes/FileHandler.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace RecepyWebsight.Classes
 {
-    class FileHandler
+    public static class FileHandler
     {
+        public static void AddAdmin()
+        {
+            string filePath = @"..\..\TextFiles\Admin.txt";
+
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(filePath))
+                {
+                    writer.WriteLine()
+                }
+            }
+            catch
+            {
+
+            }
+        }
     }
 }
