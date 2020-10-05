1b75c9d19396e627a3fdf3a3fb86368533dd3aab
Håkan Aitch Adenbäck <ha.adenback@gmail.com>
2020-10-05 16:00:09 +0200

    Main Window Design 2.0

---
+++ RecepyWebsight/Forms/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RecepyWebsight
+{
+    public partial class FrmCookBook : Form
+    {
+        public FrmCookBook()
+        {
+            InitializeComponent();
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
