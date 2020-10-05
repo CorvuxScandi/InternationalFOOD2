9139ab979f662d81e2d991c7e6fcd55e3ef067b2
Therese Berg <70585763+thereseberg@users.noreply.github.com>
2020-10-05 15:31:26 +0200

    Lagt till interface + klass RecipeType. Redigerat Recipe-klass

--- RecepyWebsight/Classes/Recipe.cs
+++ RecepyWebsight/Classes/Recipe.cs
@@ -6,9 +6,36 @@ using System.Threading.Tasks;
 
 namespace RecepyWebsight.Classes
 {
-    public class Recipe
+    public class Recipe:IMaintainable
     {
         public string Name { get; set; }
         public int Id { get; set; }
+        public List<string> Ingredients { get; set; }
+        public List<string> Instructions { get; set; }
+        public RecipeType Type { get; set; }
+
+        public Recipe(string name, int id, List<string> ingredients, List<string> instructions, RecipeType type)
+        {
+            Name = name;
+            Id = id;
+            Ingredients = ingredients;
+            Instructions = instructions;
+            Type = type; 
+        }
+
+        public void Save()
+        {
+            
+        }
+
+        public void Update()
+        {
+           
+        }
+
+        public void Delete()
+        {
+            
+        }
     }
 }
