a47dd19ce1f938d79356361975ac358e9e241e2f
Kastbe <67763895+Kastbe@users.noreply.github.com>
2020-10-07 11:12:57 +0200

    AddAdmin & Login i FileHandler

--- RecepyWebsight/Classes/FileHandler.cs
+++ RecepyWebsight/Classes/FileHandler.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 
 namespace RecepyWebsight.Classes
@@ -11,19 +7,79 @@ namespace RecepyWebsight.Classes
     {
         public static void AddAdmin(Admins admin)
         {
-            string filePath = @"..\..\TextFiles\Admin.txt";
+            string filePath = @"..\..\TextFiles\Admins.txt";
+            string line;
+            bool userExist = false;
 
             try
             {
-                using (StreamWriter writer = new StreamWriter(filePath))
+                using (StreamReader reader = new StreamReader(filePath))
                 {
-                    writer.WriteLine(admin.Name + "," + admin.Password + "," + admin.Email);
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        string[] adminCheck = line.Split(',');
+
+                        if (adminCheck[0] == admin.Name)
+                        {
+                            userExist = true;
+                            break;
+                        }
+                    }
                 }
             }
-            catch
+            catch (Exception ex)
             {
-
+                // TODO: Exception
             }
+
+            if (userExist)
+            {
+                // TODO: Exception [En användare med det namnet existerar redan]
+            }
+            else
+            {
+                try
+                {
+                    using (StreamWriter writer = new StreamWriter(filePath, true))
+                    {
+                        writer.WriteLine(admin.Name + "," + admin.Password + "," + admin.Email);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // TODO: Exception
+                }
+            }
+        }
+
+        public static bool Login(string username, string password)
+        {
+            string filePath = @"..\..\TextFiles\Admins.txt";
+            string line;
+            bool loginSuccessful = false;
+
+            try
+            {
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        string[] adminCheck = line.Split(',');
+
+                        if (adminCheck[0] == username && adminCheck[1] == password)
+                        {
+                            loginSuccessful = true;
+                            break;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // TODO: Exception
+            }
+
+            return loginSuccessful;
         }
 
         public static void AddRecpie(Recipe recipe)
