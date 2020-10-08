f51e04752231093853d2088fc90ff28ea0bd96c0
Therese Berg <70585763+thereseberg@users.noreply.github.com>
2020-10-08 09:55:32 +0200

    Redigerat sök-klass

--- RecepyWebsight/Classes/Serch.cs
+++ RecepyWebsight/Classes/Serch.cs
@@ -8,5 +8,27 @@ namespace RecepyWebsight.Classes
 {
     public class Serch
     {
+        List<Recipe> recipeList = FileHandler.GetRecipes();
+
+        public List<Recipe> Search(string recipeType, string condition)
+        {
+            List<Recipe> searchResult;
+
+            if (recipeType == "")
+            {
+                searchResult = recipeList.Where(recipe => recipe.Name.ToLower().Contains(condition.ToLower()) || 
+                                                recipe.Ingredients.Contains(condition.ToLower()))
+                                                .OrderBy(recipe => recipe.Name).ToList();
+            }
+            else
+            {
+                searchResult = recipeList.Where(recipe => (recipe.Name.ToLower().Contains(condition.ToLower()) || 
+                                                           recipe.Ingredients.Contains(condition.ToLower())) && 
+                                                           recipe.Type.Type == recipeType)
+                                                           .OrderBy(recipe => recipe.Name).ToList();
+            }
+
+            return searchResult;
+        }
     }
 }
